76793fa45efae976d5e859a86a7aee4585fe2703
James Royer <walkingalchemy@gmail.com>
2017-11-03 04:55:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -105,6 +105,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
+all_supplies_in_holidays(holiday_hash)
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
