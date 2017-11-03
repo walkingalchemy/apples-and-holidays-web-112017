f2a2b7bbadae448ec0cb490569948955a00800a6
James Royer <walkingalchemy@gmail.com>
2017-11-03 05:25:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{seasons.to_s.capitalize}:"
     holidays.each do |holiday, supply_list|
       h_string = holiday.to_s.split("_").map {|word| word.capitalize}.join(" ")
-      s_string = "   #{h_string}: "
+      s_string = "  #{h_string}: "
       supply_list.each_with_index do |supply, index|
         if index == 0
           s_string << "#{supply}"
